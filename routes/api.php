15e6d633e6b3e2d08b91f7c2d3f0cdd8d288a598
Nicolas Dobrowolski <nico.dobrowolski@gmail.com>
2022-03-09 18:53:48 +0100

    Add new route tasks

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,19 @@ use App\Http\Controllers\API\BlogController;
 */
 
 
-Route::apiResource("blog", BlogController::class); // Les routes "users.*" de l'API
+Route::get('/blogs', function () {
+    return response()->json([
+        ['titre' => 'Ansible', 'description' => 'Deploy API Laravel & React App','url'=>'https://www.ansible.com/hs-fs/hubfs/Images/blog-social/blog_ansible-and-kubernetes-c.png?width=1024&name=blog_ansible-and-kubernetes-c.png'],
+        ['titre' => 'Lab', 'description' => 'This lab is available here  https://github.com/Nico-Dobrowolski/ansible.git', 'url' => 'https://opensource.com/sites/default/files/styles/image-full-size/public/lead-images/science_experiment_beaker_lab.png?itok=plKWRhlU']
+    ]);
+});
+
+Route::get('/tasks ', function () {
+    return response()->json([
+        "Know how to use Ansible",
+        "Present the first lab",
+        "Present the second lab",
+        "Realise the conclusion",
+    ]);
+});
+
